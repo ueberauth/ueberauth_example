b4f4991760a797ba7cbc56467fac5d32f0acbfa1
Sean Callan <seancallan@gmail.com>
2015-11-15 15:30:58 -0800

    Prepare for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,8 +2,10 @@ use Mix.Config
 
 config :ueberauth_example, UeberauthExample.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "ueberauth-example.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
