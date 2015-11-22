4af90b0d769de7cdd79b18e9d0082d8261ef4b07
Daniel Neighman <has.sox@gmail.com>
2015-11-22 10:52:29 -0800

    Refactor the handling of the ueberauth auth struct in the controller

---
+++ web/models/user_from_auth.ex
@@ -0,0 +1,31 @@
+defmodule UserFromAuth do
+  alias Ueberauth.Auth
+  alias Ueberauth.Auth.Credentials
+
+  def find_or_create(%Auth{provider: :identity} = auth) do
+    case validate_password(auth.credentials) do
+      :ok ->
+        {:ok, %{id: auth.uid, name: name_from_auth(auth), avatar: auth.info.image}}
+      { :error, reason } -> {:error, reason}
+    end
+  end
+
+  def find_or_create(%Auth{} = auth) do
+    {:ok, %{id: auth.uid, name: name_from_auth(auth), avatar: auth.info.image}}
+  end
+
+  defp name_from_auth(auth) do
+    if auth.info.name do
+      auth.info.name
+    else
+      name = [auth.info.first_name, auth.info.last_name]
+      |> Enum.filter(&(&1 != nil and &1 != ""))
+      if length(name) == 0, do: auth.info.nickname, else: name = Enum.join(name, " ")
+    end
+  end
+
+  defp validate_password(%Credentials{other: %{password: ""}}), do: {:error, "Password required"}
+  defp validate_password(%Credentials{other: %{password: pw, password_confirmation: pw}}), do: :ok
+  defp validate_password(%Credentials{other: %{password: _}}), do: { :error, "Passwords do not match" }
+  defp validate_password(_), do: {:error, "Password Required"}
+end
