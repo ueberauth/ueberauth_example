9b6db8756f0427f65b0f03ff315b262f2d6d809a
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-08 18:32:39 -0700

    Remove warning getting depencencies.

--- mix.exs
+++ mix.exs
@@ -71,6 +71,6 @@ defmodule UeberauthExample.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     test: ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
