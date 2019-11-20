ffe6a5832ac1b4760cebfba09a4f06f83738e59a
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-11-20 08:01:05 -0700

    Revert "Remove as it was part of travis test configuration."
    This reverts commit e9b67f51a816f1ac0a9190274e05d3b9cbd1137e.

---
+++ config/test.exs
@@ -0,0 +1,17 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :ueberauth_example, UeberauthExampleWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :ueberauth_example, UeberauthExample.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "ueberauth_example_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
