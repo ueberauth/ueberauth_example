9fdbabee214db9cafc14b0acfdf8bee5295a5dc2
Alecia Vogel <hello@alecia.ca>
2020-01-31 16:16:46 -0700

    Update phoenix to 1.4.9 and change logout button to a regular link
    Append .env to the gitignore
    
    Format files
    
    Remove console check from my initial debugging steps for the logout button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+import css from '../css/app.css';
 // Brunch automatically concatenates all files in your
 // watched paths. Those paths can be configured at
 // config.paths.watched in "brunch-config.js".
@@ -11,7 +12,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "deps/phoenix_html/web/static/js/phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
--- config/config.exs
+++ config/config.exs
@@ -21,6 +21,9 @@ config :logger, :console,
 # Configure the Ecto Repos
 config :ueberauth_example, ecto_repos: [UeberauthExample.Repo]
 
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
